acaddc8b40586de158bb3de27579a848d31412d7
heroes75 <123456789+odin@users.noreply.github.com>
2026-06-22 04:52:30 +0000

    finish the signupPAge totaly

--- src/Conponents/SignupPage.jsx
+++ src/Conponents/SignupPage.jsx
@@ -1,11 +1,15 @@
-import { useState } from "react"
+import { useRef, useState } from "react"
 import styles from "../Styles/SignupPage.module.css"
+import { useNavigate } from "react-router"
 
 function SignupPage() {
     
     const [username, setUsername] = useState('')
     const [password, setPassword] = useState('')
     const [confirmPassword, setConfirmPassword] = useState('')
+    const [backendMsgError, setBackendMsgError] = useState([])
+    const form = useRef(null)
+    const navigate = useNavigate()
     const isUsernameAlphaNumeric = /\w+/g.test(username)
     const isUsernameOverflowThree = username.length >=3
     const isPasswordContainsOneLowerCase = /[a-z]+/g.test(password)
@@ -15,20 +19,18 @@ function SignupPage() {
     const isPasswordOverflowHeight = password.length >= 8
     const isConfirmPasswordIsEqualToPassword = password === confirmPassword
     const msgError = [
-        {status: isUsernameAlphaNumeric, msg:'Your username must be alphanumeric',},
+        {status: isUsernameAlphaNumeric, msg:'Your username must be an alphanumeric',},
         {status: isUsernameOverflowThree ,msg:'Your username must overflow 3 characters'},
         {status: isPasswordOverflowHeight ,msg: 'Your password must overflow 8 characters'},
-        {status: isPasswordContainsOneNumber ,msg: 'your password must contains at least one number'},
-        {status: isPasswordContainsOneUpperCase, msg: 'your password must contains at least one Upper-case'},
-        {status: isPasswordContainsOneLowerCase,msg: 'your password must contains at least one Lower-case'},
-        {status: isPasswordContainsOneAlphanumericCharacter, msg: 'your password must contains at least one non-alphanumeric character'},
+        {status: isPasswordContainsOneNumber ,msg: 'Your password must contains at least one number'},
+        {status: isPasswordContainsOneUpperCase, msg: 'Your password must contains at least one Upper-case'},
+        {status: isPasswordContainsOneLowerCase,msg: 'Your password must contains at least one Lower-case'},
+        {status: isPasswordContainsOneAlphanumericCharacter, msg: 'Your password must contains at least one non-alphanumeric character'},
         {status: isConfirmPasswordIsEqualToPassword, msg: 'Your confirm Password must be equal to your password'},
     ]
 
 
     function handleUsername(e) {
-        console.log('msgError:', msgError)
-        console.log('isUsernameAlphaNumeric:', isUsernameAlphaNumeric)
         setUsername(e.target.value)
     }
 
@@ -42,16 +44,29 @@ function SignupPage() {
 
     function handleSubmit(e) {
         e.preventDefault()
-        console.log('e.target.checkValidity():', e.target.checkValidity())
+        if(!form.current.checkValidity() || !isConfirmPasswordIsEqualToPassword) return
+        fetch(import.meta.env.VITE_API_URL + '/signup', {
+            method: 'POST',
+            body: JSON.stringify({username, password, confirmPassword}),
+            headers: {
+                'Content-Type': 'application/json',
+            }
+        }).
+            then(res => res.json()).
+            then(res => {
+                if(res.msg) return setBackendMsgError(res.msg)
+                navigate('/login')
+            })
     }
 
     return (
         <>
             <main>
 
-                <form>
+                <form ref={form}>
                     <ul>
-                        {msgError.map(error => error.status || <li style={{color: 'red'}}>{error.msg}</li>)}
+                        {msgError.map(error => error.status || <li key={error.msg} style={{color: 'red'}}>{error.msg}</li>)}
+                        {backendMsgError.map(msg => <li>{msg}</li>)}
                     </ul>
                     <div className={styles.inputContainer}>
                         <label htmlFor="username">Enter your username</label>
@@ -59,13 +74,13 @@ function SignupPage() {
                     </div>
                     <div className={styles.inputContainer}>
                         <label htmlFor="password">Enter your password</label>
-                        <input onChange={handlePassword} value={password} minLength={8} pattern="(?=.*[A-Z])(?=.*[a-z])(?=.*[0-9])(?=.*\W).+" type="text" name="password" id="password" placeholder="Enter your password" required />
+                        <input onChange={handlePassword} value={password} minLength={8} pattern="(?=.*[A-Z])(?=.*[a-z])(?=.*[0-9])(?=.*\W).+" type="password" name="password" id="password" placeholder="Enter your password" required />
                     </div>
-                    <div className="inputContainer">
+                    <div className={styles.inputContainer}>
                         <label htmlFor="confirmPassword">Confirm your password</label>
-                        <input onChange={handleConfirmPassword} value={confirmPassword} minLength={8} type="password" name="confirmPassword" id="confirmPassword" placeholder="Confirm your password" />
+                        <input onChange={handleConfirmPassword} value={confirmPassword} minLength={8} type="password" name="confirmPassword" id="confirmPassword" placeholder="Confirm your password" required />
                     </div>
-                    <button onClick={handleSubmit}>Submit</button>
+                    <button type="submit" onClick={handleSubmit}>Submit</button>
                 </form>
             </main>
         </>
--- src/tests/SignupPage.test.jsx
+++ src/tests/SignupPage.test.jsx
@@ -1,23 +1,41 @@
 import { screen, render } from "@testing-library/react";
-import { describe, test, expect } from "vitest";
-
+import { describe, test, expect, vi } from "vitest";
+import { createMemoryRouter, RouterProvider } from "react-router";
 import SignupPage from "../Conponents/SignupPage";
 import userEvent from "@testing-library/user-event";
+import routes from '../../routes'
+
+
+const navigate = vi.fn()
+vi.mock(import('react-router'), async (importOriginal) => {
+    const module = await importOriginal()
+    return {
+        ...module,
+        useNavigate: () => navigate
+    }
+})
+
+
+
+const router = createMemoryRouter(routes, {
+    initialEntries: ['/signup']
+})
 
 describe('should have three inputs components and a button', () => {
     test('a input username with his label', () => {
-    render(<SignupPage/>)
+    // render(<SignupPage/>)
+    render(<RouterProvider router={router} />)
         expect(screen.getByRole('textbox', {name: 'Enter your username'})).toBeInTheDocument()
         expect(screen.getByLabelText('Enter your username')).toBeInTheDocument()
     })
     test('a input password and his label', () => {
         render(<SignupPage/>)
-        expect(screen.getByRole('textbox', {name: 'Enter your password'})).toBeInTheDocument()
+        expect(screen.getByPlaceholderText('Enter your password')).toBeInTheDocument()
         expect(screen.getByLabelText('Enter your password')).toBeInTheDocument()
     })
     test('a confirmPassword input and his label', () => {
         render(<SignupPage/>)
-        expect(screen.getByRole('textbox', {name: 'Confirm your password'})).toBeInTheDocument()
+        expect(screen.getByPlaceholderText('Confirm your password')).toBeInTheDocument()
         expect(screen.getByLabelText('Confirm your password')).toBeInTheDocument()
     })
     test('a button', () => {
@@ -26,14 +44,67 @@ describe('should have three inputs components and a button', () => {
     })
 })
 
-describe('should be display the right message error', () => {
-    test('if user type no input should be have an error', async () => {
-        expect(screen.getByText('Your username must be alphanumeric'))
+describe('should be display the right message error', async () => {
+    test('if user type no input should be have these following errors', async () => {
+        render(<SignupPage/>)
+        const user = userEvent.setup()
+        const button = screen.getByRole('button')
+        await user.click(button)
+        expect(screen.getByText('Your username must be an alphanumeric'))
+        expect(screen.getByText('Your username must overflow 3 characters'))
+        expect(screen.getByText('Your password must contains at least one number'))
+        expect(screen.getByText('Your password must contains at least one Upper-case'))
+        expect(screen.getByText('Your password must contains at least one Lower-case'))
         expect(screen.getByText('Your password must overflow 8 characters'))
-        expect(screen.getByText('your password must contains at least one non-alphanumeric character'))
+        expect(screen.getByText('Your password must contains at least one non-alphanumeric character'))
 
     })
-    test('if user type the wrong input it\'s should be display the message error', () => {
+    test('if user type the wrong input it\'s should be display the message error', async () => {
+        render(<SignupPage/>)
+        const user =  userEvent.setup()
+        const inputUsername = screen.getByRole('textbox', {name: 'Enter your username'})
+        const inputPassword = screen.getByText('Enter your password')
+        const inputConfirmPassword = screen.getByText('Confirm your password')
+        const button = screen.getByRole('button')
+        await user.type(inputUsername, 'q')
+        await user.type(inputPassword, '1a')
+        await user.type(inputConfirmPassword, '1')
+        await user.click(button)
+        expect(screen.getByText('Your username must overflow 3 characters'))
+        expect(screen.getByText('Your password must contains at least one Upper-case'))
+        expect(screen.getByText('Your password must contains at least one non-alphanumeric character'))
+        expect(screen.getByText('Your password must overflow 8 characters'))
+        expect(screen.getByText('Your confirm Password must be equal to your password'))
+    })
+})
 
+describe('if user type the right i\'s should be display redirect', () => {
+    test('the user type an existing username', async () => {
+        window.fetch = vi.fn(() => Promise.resolve(({json: () => ({msg: ['this username already exist']})})))
+        render(<SignupPage/>)
+        const user =  userEvent.setup()
+        const inputUsername = screen.getByRole('textbox', {name: 'Enter your username'})
+        const inputPassword = screen.getByText('Enter your password')
+        const inputConfirmPassword = screen.getByText('Confirm your password')
+        const button = screen.getByRole('button')
+        await user.type(inputUsername, 'qqq')
+        await user.type(inputPassword, '1234567@Aa')
+        await user.type(inputConfirmPassword, '1234567@Aa')
+        await user.click(button)
+        expect(screen.getByText('this username already exist')).toBeInTheDocument()
+    })
+    test('the user type the right input', async () => {
+        window.fetch = vi.fn(() => Promise.resolve(({json: () => ({user: ''})})))
+        render(<SignupPage/>)
+        const user =  userEvent.setup()
+        const inputUsername = screen.getByRole('textbox', {name: 'Enter your username'})
+        const inputPassword = screen.getByText('Enter your password')
+        const inputConfirmPassword = screen.getByText('Confirm your password')
+        const button = screen.getByRole('button')
+        await user.type(inputUsername, 'qqq')
+        await user.type(inputPassword, '1234567@Aa')
+        await user.type(inputConfirmPassword, '1234567@Aa')
+        await user.click(button)
+        expect(navigate).toHaveBeenCalledOnce()
     })
 })
